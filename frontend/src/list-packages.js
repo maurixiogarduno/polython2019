78ae3d7e70387910c75e7de03a809532be93b454
Jorge Hernández <jorgeantonio.hernandez.guzman.contractor@bbva.com>
2019-03-02 23:47:07 -0600

    update(*): new folder to styles, add dinamic list items component

--- frontend/src/list-packages.js
+++ frontend/src/list-packages.js
@@ -19,7 +19,7 @@ class ListPackages extends PolymerElement {
           <p>Modus commodo minimum eum te, vero utinam assueverit per eu.</p>
           <p>Ea duis bonorum nec, falli paulo aliquid ei eum.Has at minim mucius aliquam, est id tempor laoreet.Pro saepe pertinax ei, ad pri animal labores suscipiantur.</p>
         </div>
-        <list-items></list-items>
+        <list-items list-items='[{"name":"Plomeria","status":"disable"}, {"name":"Ambulancia", "status":"disable"}]'></list-items>
       `;
     }
 }
